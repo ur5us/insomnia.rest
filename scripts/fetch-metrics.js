812eacd0f0614f6fe43a8173c6d94133951927ec
Gregory Schier <gschier1990@gmail.com>
2018-03-31 09:34:42 -0400

    Update transparency page

--- scripts/fetch-metrics.js
+++ scripts/fetch-metrics.js
@@ -1,5 +1,6 @@
 const request = require('request');
 const fs = require('fs');
+const path = require('path');
 
 const pathname = process.argv[2];
 
@@ -8,23 +9,38 @@ if (!pathname) {
   process.exit(1);
 }
 
-const d = new Date();
-const endDate = [d.getUTCFullYear(), d.getUTCMonth() + 1, d.getUTCDate()].map(n => (
-  n.toString().length === 1 ? `0${n}` : n.toString()
-)).join('-');
 
-const options = {
-  method: 'GET',
-  url: 'https://api.baremetrics.com/v1/metrics',
-  qs: {start_date: '2016-12-01', end_date: endDate},
-  headers: {'Authorization': `Bearer ${process.env.BAREMETRICS_KEY}`}
-};
+(async function run () {
+  const baremetricsData = await fetchBaremetrics();
+  const gaData = await fetchGA();
+  // fs.writeFileSync(pathname, `window.metrics = ${formattedBody}`);
+})();
 
-request(options, function (err, response, body) {
-  const formattedBody = JSON.stringify(JSON.parse(body), null, '\t');
-  fs.writeFileSync(pathname, `window.metrics = ${formattedBody}`);
-  if (response.statusCode !== 200) {
-    throw new Error('Metrics request failed: ' + response.body);
-  }
-  console.log(`Metrics written to ${pathname}`)
-});
+function fetchBaremetrics () {
+  return new Promise((resolve, reject) => {
+
+    const d = new Date();
+    const endDate = [d.getUTCFullYear(), d.getUTCMonth() + 1, d.getUTCDate()].map(n => (
+      n.toString().length === 1 ? `0${n}` : n.toString()
+    )).join('-');
+
+    const options = {
+      method: 'GET',
+      url: 'https://api.baremetrics.com/v1/metrics',
+      qs: {start_date: '2016-12-01', end_date: endDate},
+      headers: {'Authorization': `Bearer ${process.env.BAREMETRICS_KEY}`}
+    };
+
+    request(options, function (err, response, body) {
+      if (response.statusCode !== 200) {
+        return reject(new Error('Metrics request failed: ' + response.body));
+      }
+
+      resolve(JSON.parse(body));
+    });
+  })
+}
+
+function fetchGA () {
+
+}
---
+++ site/content/transparency.md
@@ -0,0 +1,15 @@
+---
+title: Transparency
+subTitle: Sharing the Road to Insomnia
+slug: transparency
+type: transparency
+---
+
+The purpose of this page is to give back to the indie hacker community by sharing
+the details of Insomnia's progress over time.
+There's also an
+[Indie Hackers Interview](https://www.indiehackers.com/product/insomnia)
+covering process from taking Insomnia from a side project to a revenue-generating product if
+you'd like to know more.
+
+## Summary
