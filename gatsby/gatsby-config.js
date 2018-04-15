651c9c354d97333315cabd31ad3b7d8c36c10f85
Gregory Schier <gschier1990@gmail.com>
2018-04-15 10:52:05 -0400

    Favicons

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -21,6 +21,24 @@ module.exports = {
       },
     },
     {
+      resolve: 'gatsby-plugin-favicon',
+      options: {
+        logo: './src/assets/favicon.png',
+        injectHTML: true,
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          twitter: false,
+          yandex: false,
+          windows: false
+        }
+      }
+    },
+    {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
         trackingId: 'UA-9837747-12',
