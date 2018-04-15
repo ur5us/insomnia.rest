2a8e86acba7e5086db73bb46b3fb4e55dacb9fc6
Gregory Schier <gschier1990@gmail.com>
2018-04-15 10:54:30 -0400

    Try inline images

--- gatsby/src/components/social-cards.js
+++ gatsby/src/components/social-cards.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 import {withPrefix} from 'gatsby-link';
+import cardIconSrc from '../assets/card-icon.png';
 
 export default ({title, summary}) => (
   <Helmet>
@@ -8,11 +9,11 @@ export default ({title, summary}) => (
     <meta name="twitter:site" content="@GetInsomnia"/>
     <meta name="twitter:title" content={title}/>
     <meta name="twitter:description" content={summary}/>
-    <meta name="twitter:image" content={withPrefix('/images/twitter-card-icon.png')}/>
+    <meta name="twitter:image" content={cardIconSrc}/>
 
     <meta property="og:type" content="article"/>
     <meta property="og:title" content={title}/>
     <meta property="og:description" content={summary}/>
-    <meta property="og:image" content={withPrefix('/images/twitter-card-icon.png')}/>
+    <meta property="og:image" content={cardIconSrc}/>
   </Helmet>
 );
