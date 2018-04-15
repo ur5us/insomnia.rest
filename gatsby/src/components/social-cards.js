b5bb8b024980f1e4d05d9525682edeb457e6634e
Gregory Schier <gschier1990@gmail.com>
2018-04-15 10:59:31 -0400

    Try fix

--- gatsby/src/components/social-cards.js
+++ gatsby/src/components/social-cards.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import Helmet from 'react-helmet';
-import {withPrefix} from 'gatsby-link';
-import cardIconSrc from '../assets/card-icon.png';
 
 export default ({title, summary}) => (
   <Helmet>
@@ -9,11 +7,11 @@ export default ({title, summary}) => (
     <meta name="twitter:site" content="@GetInsomnia"/>
     <meta name="twitter:title" content={title}/>
     <meta name="twitter:description" content={summary}/>
-    <meta name="twitter:image" content={cardIconSrc}/>
+    <meta name="twitter:image" content="https://insomnia.rest/images/twitter-card-icon.png"/>
 
     <meta property="og:type" content="article"/>
     <meta property="og:title" content={title}/>
     <meta property="og:description" content={summary}/>
-    <meta property="og:image" content={cardIconSrc}/>
+    <meta property="og:image" content="https://insomnia.rest/images/twitter-card-icon.png"/>
   </Helmet>
 );
