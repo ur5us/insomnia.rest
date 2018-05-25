f88ff9f64fbb07d3a819c689dc7b9847cbe81688
Gregory Schier <gschier1990@gmail.com>
2018-05-24 22:16:28 -0400

    Update download page

--- src/pages/download.js
+++ src/pages/download.js
@@ -36,9 +36,7 @@ export default class extends React.Component {
               <div className="col-4 platform-download">
                 <i className="platform-download__icon apple"/>
                 <p>
-                  <Link to={links.builds.mac}
-                        data-ga="Download:Mac"
-                        className="button">
+                  <Link to={links.builds.mac} className="button">
                     <svg className="icon" viewBox="0 0 512 512">
                       <path
                         d="M216 0h80c13.3 0 24 10.7 24 24v168h87.7c17.8 0 26.7 21.5 14.1 34.1L269.7 378.3c-7.5 7.5-19.8 7.5-27.3 0L90.1 226.1c-12.6-12.6-3.7-34.1 14.1-34.1H192V24c0-13.3 10.7-24 24-24zm296 376v112c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24V376c0-13.3 10.7-24 24-24h146.7l49 49c20.1 20.1 52.5 20.1 72.6 0l49-49H488c13.3 0 24 10.7 24 24zm-124 88c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20zm64 0c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20z"/>
@@ -54,9 +52,7 @@ export default class extends React.Component {
               <div className="col-4 platform-download">
                 <i className="platform-download__icon windows"/>
                 <p>
-                  <Link to={links.builds.windows}
-                        data-ga="Download:Windows"
-                        className="button">
+                  <Link to={links.builds.windows} className="button">
                     <svg className="icon" viewBox="0 0 512 512">
                       <path
                         d="M216 0h80c13.3 0 24 10.7 24 24v168h87.7c17.8 0 26.7 21.5 14.1 34.1L269.7 378.3c-7.5 7.5-19.8 7.5-27.3 0L90.1 226.1c-12.6-12.6-3.7-34.1 14.1-34.1H192V24c0-13.3 10.7-24 24-24zm296 376v112c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24V376c0-13.3 10.7-24 24-24h146.7l49 49c20.1 20.1 52.5 20.1 72.6 0l49-49H488c13.3 0 24 10.7 24 24zm-124 88c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20zm64 0c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20z"/>
@@ -72,9 +68,7 @@ export default class extends React.Component {
               <div className="col-4 platform-download">
                 <i className="platform-download__icon linux bg-linux"/>
                 <p>
-                  <Link to="https://support.insomnia.rest/article/23-installation#ubuntu"
-                        data-ga="Download:Linux:Ubuntu"
-                        className="button">
+                  <Link to="https://support.insomnia.rest/article/23-installation#ubuntu" className="button">
                     <svg className="icon" viewBox="0 0 512 512">
                       <path
                         d="M216 0h80c13.3 0 24 10.7 24 24v168h87.7c17.8 0 26.7 21.5 14.1 34.1L269.7 378.3c-7.5 7.5-19.8 7.5-27.3 0L90.1 226.1c-12.6-12.6-3.7-34.1 14.1-34.1H192V24c0-13.3 10.7-24 24-24zm296 376v112c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24V376c0-13.3 10.7-24 24-24h146.7l49 49c20.1 20.1 52.5 20.1 72.6 0l49-49H488c13.3 0 24 10.7 24 24zm-124 88c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20zm64 0c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20z"/>
@@ -84,10 +78,12 @@ export default class extends React.Component {
                   </Link>
                 </p>
                 <p className="subtle small" style={{maxWidth: '12rem', margin: 'auto'}}>
-                  or
-                  {' '}
-                  <Link to="https://support.insomnia.rest/article/23-installation#linux" data-ga="Download:Linux:AUR">
-                    Other Linux Distros
+                  or&nbsp;
+                  <code style={{lineBreak: 'no-wrap'}}>sudo snapcraft install insomnia</code>
+                  <br/>
+                  or&nbsp;
+                  <Link to="https://support.insomnia.rest/article/23-installation#linux">
+                    view other methods
                   </Link>
                 </p>
               </div>
